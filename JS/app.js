a02b59177525647ba48b0d477d771349f2166431
Naira Martins <nairarivelli@gmail.com>
2022-10-29 14:19:51 -0300

    commit inicial

---
+++ JS/app.js
@@ -0,0 +1,30 @@
+const teste6 = () => {
+    $('p').click(function (e) {
+        $('i').toggleClass('fa fa-plus fa fa-minus');   
+    });
+}
+teste6();
+const teste7 = () => {
+    $('h2').click(function (e) {
+        $('i2').toggleClass('fa fa-plus fa fa-minus'); 
+    });
+}
+teste7();
+const teste8 = () => {
+    $('h3').click(function (e) {
+        $('i3').toggleClass('fa fa-plus fa fa-minus');  
+    });
+}
+teste8();
+const teste9 = () => {
+    $('h4').click(function (e) {
+        $('i4').toggleClass('fa fa-plus fa fa-minus');  
+    });
+}
+teste9();
+const teste10 = () => {
+    $('h5').click(function (e) {
+        $('i5').toggleClass('fa fa-plus fa fa-minus'); 
+    });
+}
+teste10();
